216c7977862600686e9f5b225b2b43e48715ec9b
oopfff <uhoh.yip@gmail.com>
2025-02-08 10:54:35 -0500

    Update inject.js

--- extension/inject.js
+++ extension/inject.js
@@ -1,4 +1,3 @@
-// Create the popup container (DIV)
 var mydiv = document.createElement("div");
 mydiv.id = "mydiv";
 mydiv.style.position = "absolute";
@@ -7,27 +6,10 @@ mydiv.style.backgroundColor = "#f69420";
 mydiv.style.textAlign = "center";
 mydiv.style.border = "1px solid #f69420";
 mydiv.style.width = "270px";
-mydiv.style.top = "69px"; // Position it 10px from the top
-mydiv.style.right = "0px"; // Position it 10px from the right
+mydiv.style.top = "69px";
+mydiv.style.right = "0px";
 
-// Create the header of the popup
-var mydivheader = document.createElement("div");
-mydivheader.id = "mydivheader";
-
-// Add the header to the popup
-mydiv.appendChild(mydivheader);
-
-// Append the popup to the body
 document.body.appendChild(mydiv);
 
-// Fetch the content from the URL and set it as the innerHTML of the popup
-fetch('https://raw.githubusercontent.com/oopfff/clementyne/refs/heads/main/extension/main.html')
-  .then(response => response.text())
-  .then(data => {
-    mydiv.innerHTML += data; // Append the fetched content to the popup
-    // Reinitialize drag after content is loaded
-    dragElement(mydiv);
-  })
-  .catch(error => {
-    console.log('Error fetching the HTML content:', error);
-  });
+// ugh so much styling but this just adds a button that looks good and can run clementyne
+mydiv.innerHTML = "<style>#mydiv {background-color: #f69420;color: black;font-weight: bold;padding-left: 10px;padding-right: 10px;}#mydiv button {transition: all 0.25s ease;background-color: #fcd7ab;border-radius: 0.25rem;font-family:cursive;}#mydiv button:hover {transition: all 0.25s ease;background-color: #fdeedc;}#mydiv button:hover {cursor:cell;}</style><button onClick='oshow()'>Show Clementyne</button>";
