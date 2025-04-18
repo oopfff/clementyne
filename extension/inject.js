0a978d15e068790e6a3f288eca7a00a773dd0169
oopfff <uhoh.yip@gmail.com>
2025-04-18 16:49:42 -0400

    Update inject.js

--- extension/inject.js
+++ extension/inject.js
@@ -5,11 +5,11 @@ mydiv.style.zIndex = 9;
 mydiv.style.backgroundColor = "#f69420";
 mydiv.style.textAlign = "center";
 mydiv.style.border = "1px solid #f69420";
+mydiv.style.borderRadius = "6.9px";
 mydiv.style.width = "270px";
 mydiv.style.top = "69px";
 mydiv.style.right = "0px";
 
 document.body.appendChild(mydiv);
 
-// ugh so much styling but this just adds a button that looks good and can run clementyne
 mydiv.innerHTML = "<style>#mydiv {background-color: #f69420;color: black;font-weight: bold;padding-left: 10px;padding-right: 10px;}#mydiv button {transition: all 0.25s ease;background-color: #fcd7ab;border-radius: 0.25rem;font-family:cursive;}#mydiv button:hover {transition: all 0.25s ease;background-color: #fdeedc;}#mydiv button:hover {cursor:cell;}</style><button onClick='oshow()'>Show Clementyne</button>";
